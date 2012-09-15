0317bd09898087bed83d2c45e899c4575e2d5076
tallevy <tallevy@usc.edu>
2012-09-14 20:51:54 -0700

    Skeleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FDMLS::Application.config.secret_token = '7b8c8eff9a0d1ef145174cccc00e00e83e0cc0c7b8a67718edfe0a6ca81ae6168439f16f344a03fecd29950f489a5b65843b68fc0aa12f28f62eb680cc71a837'
